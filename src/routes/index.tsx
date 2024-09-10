8790534dd7d381a35663c6a8b51146f8d5f445ff
aaron <aaronlyn88@gmail.com>
2024-09-10 15:57:49 +0800

    feat: added shadcn components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,23 +3,23 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
   return (
-    <>
+    <div class="mx-auto max-w-[692px] overflow-x-hidden px-6 py-12 text-gray-1200 antialiased sm:py-32 md:overflow-x-visible md:py-16">
       <h1>Hi 👋</h1>
       <div>
         Can't wait to see what you build with qwik!
         <br />
         Happy coding.
       </div>
-    </>
+    </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "K",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "K's home page",
     },
   ],
 };
